fe195809377b68e0f08b2f28139a03f1358e66e8
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-15 10:36:04 +0200

    changed properties adding NATIVE

--- src/main/java/com/acoustic/controller/PensionZusController.java
+++ src/main/java/com/acoustic/controller/PensionZusController.java
@@ -22,12 +22,13 @@ public class PensionZusController {
 
     public static final String DESCRIPTION = "description";
     public static final String VALUE = "value";
+    public static final int MINIMUM_SALARY = 2000;
     private final PensionZusRepository pensionZusRepository;
     private final SalaryCalculatorService salaryCalculatorService;
 
 
     @PostMapping("/calculation/{grossMonthlySalary}")
-    public Map<String, String> calculatePensionZus(@PathVariable @Min(2000) BigDecimal grossMonthlySalary) {
+    public Map<String, String> calculatePensionZus(@PathVariable @Min(MINIMUM_SALARY) BigDecimal grossMonthlySalary) {
         var pensionZus = this.salaryCalculatorService.apply(grossMonthlySalary);
         this.pensionZusRepository.save(PensionZus.builder().pensionZusAmount(pensionZus).build());
         return Map.of(DESCRIPTION, this.salaryCalculatorService.getDescription(), VALUE, String.valueOf(pensionZus));
